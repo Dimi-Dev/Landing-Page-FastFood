a80e135de30ecd898be685b6fbb9b99c28adbc5b
Dimitri <dimitri.devanne24@gmail.com>
2021-08-24 19:08:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const slides = document.querySelectorAll('.slides')
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+
+i = 0;
+
+function ActiveSlide(n){
+    for(slide of slides)
+    slide.classList.remove('active')
+    slides[n].classList.add('active')
+}
+
+prev.addEventListener('click', () => {
+    if(i == 0){
+        i = slides.length -1;
+        ActiveSlide(i)
+    } else{
+        i--
+        ActiveSlide(i)
+    }
+})
+
+next.addEventListener('click', () => {
+    if(i == slides.length -1){
+        i = 0;
+        ActiveSlide(i)
+    } else{
+        i++
+        ActiveSlide(i)
+    }
+})
